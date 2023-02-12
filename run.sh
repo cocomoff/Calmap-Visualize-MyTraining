a8c653f2053fb965dafa784a682ad7f8895ffa78
cocomoff <keisuke.otaki.jp@gmail.com>
2023-02-12 16:45:48 +0900

    update scripts and jogging data

---
+++ run.sh
@@ -0,0 +1,29 @@
+# 2022
+python src/main.py \
+    --data ./data/power-training.csv \
+    --start 2022-1-1 \
+    --end 2022-12-31 \
+    --single \
+    --output output/power-training.png
+
+# 2023
+python src/main.py \
+    --data ./data/power-training.csv \
+    --start 2023-1-1 \
+    --end 2023-12-31 \
+    --single \
+    --output output/power-training-2023.png
+
+# 2022
+python src/main.py \
+    --data ./data/jogging.csv \
+    --start 2022-1-1 \
+    --end 2022-12-31 \
+    --output output/jogging.png
+
+# 2023
+python src/main.py \
+    --data ./data/jogging.csv \
+    --start 2023-1-1 \
+    --end 2023-12-31 \
+    --output output/jogging-2023.png
